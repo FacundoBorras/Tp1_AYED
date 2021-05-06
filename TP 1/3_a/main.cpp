12b67aa7ff0052d82f5255ad95743723af23ec9b
FacundoBorras <83559967+FacundoBorras@users.noreply.github.com>
2021-05-06 16:29:52 -0300

    Add files via upload

---
+++ TP 1/3_a/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Ingrese un valor numerico positivo: "<<endl;
+    cin>>x;
+
+    cout << "Conversion a kmts/hr: " <<x/3.6<<"kmts/hr"<< endl;
+    return 0;
+}
---
+++ TP 1/3_b/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+
+    float a,b,c,x1,x2;
+
+    cout<<"Ingrese ax2, bx y c: "<<endl;
+    cin>>a>>b>>c;
+
+    x1=(-b+(sqrt(b*b-4*a*c)))/(2*a);
+    x2=(-b-(sqrt(b*b-4*a*c)))/(2*a);
+
+    if(2*a!=0 && (b*b-4*a*c)>=0)
+        cout<<"x1 = "<<x1<<"\nx2 = "<<x2<<endl;
+    else
+        cout<<"No tiene solucion real "<<endl;
+
+
+return 0;
+}
